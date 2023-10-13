87912e9160947a39e789b66bbed52fb4dca0def3
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-10-13 15:10:58 +0700

    mengembalikan letak info beasiswa ke informasi mahasiswa

--- resources/views/detil-evaluasi-beasiswa.blade.php
+++ resources/views/detil-evaluasi-beasiswa.blade.php
@@ -66,6 +66,15 @@
                                     </svg>
                                     Nama : <strong>Erlangga Harrys Setyawan</strong>
                                 </div>
+                                <div class="mb-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-school me-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6"></path>
+                                        <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4"></path>
+                                    </svg>
+                                    Beasiswa : <strong>Beasiswa Kuliah 0 Rupiah</strong>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -73,25 +82,7 @@
 
 
                 <div class="row mt-2">
-                    <div class="col-3">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="subheader">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-school me-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                        stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6"></path>
-                                        <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4"></path>
-                                    </svg>
-                                    Beasiswa yang diperoleh
-                                </div>
-                                <div class="h3 m-0">
-                                    Beasiswa Kuliah 0 Rupiah
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-3">
+                    <div class="col-4">
                         <div class="card">
                             <div class="card-body">
                                 <div class="subheader">
@@ -110,7 +101,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-3">
+                    <div class="col-4">
                         <div class="card">
                             <div class="card-body">
                                 <div class="subheader">
@@ -133,7 +124,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-3">
+                    <div class="col-4">
                         <div class="card">
                             <div class="card-body">
                                 <div class="subheader">
