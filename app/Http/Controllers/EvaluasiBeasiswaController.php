13eb901f5232814b77a74406a5320b9cfca87fda
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-11-09 09:43:56 +0700

    memperjelas keterangan

--- app/Http/Controllers/EvaluasiBeasiswaController.php
+++ app/Http/Controllers/EvaluasiBeasiswaController.php
@@ -18,15 +18,16 @@ class EvaluasiBeasiswaController extends Controller
         $semuaPenerima = Terima::fromQuery(Terima::queryPenerimaBeasiswa(), ['SMT' => session('semester')])
             ->load('mahasiswa', 'jenis_beasiswa1', 'jenis_beasiswa2', 'syarat_peserta.syarat');
 
-        // Hapus / Buang penerima beasiswa yang sudah dievaluasi oleh bagian yang LOGIN
+        // Hapus / Buang penerima beasiswa yang SUDAH dievaluasi oleh bagian yang LOGIN
         $semuaPenerima = $semuaPenerima->reject(function ($penerima, $key) {
-            // return FALSE : artinya penerima beasiswa tidak dibuang
-            // return TRUE : artinya penerima beasiswa dibuang
+            // return FALSE : artinya penerima beasiswa TIDAK DIBUANG
+            // return TRUE : artinya penerima beasiswa DIBUANG
 
-            // Kalau jumlah syarat_peserta nya sama dengan 0, maka return false
+            // Kalau jumlah syarat_peserta nya sama dengan 0, berarti belum ada evaluasi
             if ($penerima->syarat_peserta->count() == 0) return false;
 
-            // Kalau tidak ada syarat beasiswa yang bagian validasi nya ada bagian yang LOGIN, maka return false
+            // Kalau di dalam syarat_peserta tidak ada syarat yang bagian validasi nya adalah bagian yang LOGIN,
+            // berarti sudah ada evaluasi, tapi yang mengevaluasi bukan bagian yang LOGIN
             if ($penerima->syarat_peserta->where('syarat.bagian_validasi', auth()->user()->bagian)->count() == 0) return false;
 
             return true;
