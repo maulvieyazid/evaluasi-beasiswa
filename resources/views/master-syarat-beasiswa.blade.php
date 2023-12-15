45a9b67422ee761be38a14d7a528eeb595156534
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2023-12-15 10:30:20 +0700

    tambah datatables pada index maintenance syarat beasiswa

--- resources/views/master-syarat-beasiswa.blade.php
+++ resources/views/master-syarat-beasiswa.blade.php
@@ -2,6 +2,12 @@
 
 @section('html_title', 'Maintenance Syarat Beasiswa')
 
+@push('css')
+    <!-- Datatables Bootstrap 5 Theme -->
+    <link rel="stylesheet" href="{{ asset('assets/libs/datatables/dataTables.bootstrap5.min.css') }}" />
+@endpush
+
+
 @php
     use App\Models\Terima;
 @endphp
@@ -32,37 +38,39 @@
                 <div class="row">
                     <div class="col">
                         <div class="card">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-hover table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">Kode</th>
-                                            <th class="text-center">Nama</th>
-                                            <th class="text-center">Keterangan</th>
-                                            <th class="text-center"></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($semuaBeasiswa as $bea)
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-hover table-bordered" id="tabelBeasiswa">
+                                        <thead>
                                             <tr>
-                                                <td class="text-center">{{ $bea->kd_jenis }}</td>
-                                                <td>{{ $bea->nama }}</td>
-                                                <td>{{ $bea->keterangan }}</td>
-                                                <td>
-                                                    <a href="{{ route('detil.master-syarat-beasiswa', [$bea->kd_jenis]) }}" class="btn btn-outline-primary w-100 btn-sm">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                                            stroke-linejoin="round">
-                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                                            <path d="M9 11l3 3l8 -8"></path>
-                                                            <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9"></path>
-                                                        </svg>
-                                                        Syarat
-                                                    </a>
-                                                </td>
+                                                <th class="text-center">Kode</th>
+                                                <th class="text-center">Nama</th>
+                                                <th class="text-center">Keterangan</th>
+                                                <th class="text-center"></th>
                                             </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($semuaBeasiswa as $bea)
+                                                <tr>
+                                                    <td class="text-center">{{ $bea->kd_jenis }}</td>
+                                                    <td>{{ $bea->nama }}</td>
+                                                    <td>{{ $bea->keterangan }}</td>
+                                                    <td>
+                                                        <a href="{{ route('detil.master-syarat-beasiswa', [$bea->kd_jenis]) }}" class="btn btn-outline-primary w-100 btn-sm">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                                                                stroke-linejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                                <path d="M9 11l3 3l8 -8"></path>
+                                                                <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9"></path>
+                                                            </svg>
+                                                            Syarat
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -73,3 +81,18 @@
     </div>
 
 @endsection
+
+@push('js')
+    <!-- Datatables -->
+    <script src="{{ asset('assets/libs/datatables/jquery.dataTables.min.js') }}"></script>
+    <!-- Datatables Bootstrap 5 Theme -->
+    <script src="{{ asset('assets/libs/datatables/dataTables.bootstrap5.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#tabelBeasiswa').DataTable({
+                order: [] // <- Mematikan order saat inisialisasi
+            });
+        });
+    </script>
+@endpush
