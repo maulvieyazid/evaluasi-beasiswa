1fb00dc322d7a6bf399dadd3e903b7039b396386
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2024-02-22 09:16:26 +0700

    tambah method getSmtSelanjutnya() di model Semester

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -14,4 +14,32 @@ class Semester extends Model
     public $timestamps = false;
 
     public $incrementing = false;
+
+
+    public static function getSmtSelanjutnya($smt = null)
+    {
+        if ($smt == null) return "Mohon isikan parameter smt.";
+
+        // Kalo semester nya tidak 3 digit, maka langsung kembalikan semester nya
+        if (strlen($smt) != 3) return $smt;
+
+        // Ubah smt nya jadi int
+        $smt = (int) $smt;
+
+        // Ambil digit terakh1r semester
+        $digit_terakhir = $smt % 10;
+
+        // Nilai default untuk semester selanjutnya adalah semester
+        $smt_selanjutnya = $smt;
+
+        // Kalau digit terakhir nya adalah 1, maka untuk semester selanjutnya, tinggal menambahkan semester dengan 1
+        // Misal : 231 => 232
+        if ($digit_terakhir == 1) $smt_selanjutnya = $smt + 1;
+
+        // Kalau digit terakhir nya adalah 1, maka untuk semester selanjutnya, tinggal menambahkan semester dengan 9
+        // Misal : 242 => 251
+        if ($digit_terakhir == 2) $smt_selanjutnya = $smt + 9;
+
+        return $smt_selanjutnya;
+    }
 }
