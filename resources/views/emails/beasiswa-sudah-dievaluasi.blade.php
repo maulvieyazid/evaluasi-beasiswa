84a9e0fabed5c195ccc254a3195748433327d1b5
maulvieyazid <17469172+maulvieyazid@users.noreply.github.com>
2024-02-23 09:10:55 +0700

    membuat mailables untuk template kirim email setelah evaluasi beasiswa

---
+++ resources/views/emails/beasiswa-sudah-dievaluasi.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+    <title>Beasiswa Sudah Dievaluasi</title>
+
+    <style>
+        #main-table {
+            border: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <table id="main-table" cellpadding="0" cellspacing="0">
+        <tr>
+            <td>Beasiswa kamu sudah dievaluasi</td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
